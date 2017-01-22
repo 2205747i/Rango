e02d718f8fe9e405270925e47058094b6a14274b
2205747i <h.ivanov96@gmail.com>
2017-01-22 01:09:58 +0000

    Add files via upload
    Chapter 4

--- first/rango/urls.py
+++ first/rango/urls.py
@@ -2,6 +2,7 @@ from rango import views
 from django.conf.urls import url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about')
